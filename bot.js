283b3aa4e83a9e0d1619033dbf84e38847f7a642
jehran <rafu0155@gmail.com>
2026-09-06 13:27:47 +0600

    Fix static files

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
